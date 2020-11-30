3709ddb466385ff4a347ce00c173f9d0e6480f09
MMD <975913801@qq.com>
2020-11-30 16:30:56 +0800

    改了helloController一个方法

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -12,4 +12,10 @@ public class HelloController {
     public String hello(){
         return "sb";
     }
+
+    @RequestMapping("/good")
+    @ResponseBody
+    public String good(){
+        return "nt";
+    }
 }
